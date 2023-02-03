79255674cd4c1927c6725a167e452b42f979a85d
ryannicholas <ryannicholas.vieira@gmail.com>
2023-02-03 18:45:38 -0300

    fix: :rocket: update dist

--- dist/routes/UserRoutes.js
+++ dist/routes/UserRoutes.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const UserController_1 = require("../controllers/UserController");
-const UserValidation_1 = __importDefault(require("../middlewares/UserValidation"));
+const UserController_1 = require("../controllers/UserController.js");
+const UserValidation_1 = __importDefault(require("../middlewares/UserValidation.js"));
 const userRouter = (0, express_1.Router)();
 userRouter.post('/register', UserValidation_1.default, UserController_1.userRegister);
 userRouter.get('/user', UserController_1.findByCpf);
